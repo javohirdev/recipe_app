5822e44a78ec3c552f6be1b5e9cd497738998a43
Javohir Hakimov <57003102+javohirdev@users.noreply.github.com>
2020-10-13 15:37:19 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-// import StatLesson from './StatLesson/StatLesson';
-// import PostRequest from './components/PostRequest';
-// import Lesson1 from './components/Lesson1';
-// import Backend from './components/Backend';
 import Recipe from './app/Recipe';
-// import Project from './webProject/Project';
 
 function App() {
   return (
     <div>
-      {/* <Lesson1 
-      title="Class Component"
-      data={{name: 'Javohir', age: 16}}
-      /> */}
-      {/* <Backend /> */}
       <Recipe />
-      {/* <PostRequest /> */}
-      {/* <StatLesson /> */}
-      {/* <Project /> */}
     </div>
     
   );
